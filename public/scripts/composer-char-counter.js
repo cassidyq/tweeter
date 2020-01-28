3bc515d7547010c8ba483a590b60fd3ed58359de
Cassidy Quaite <cassidy@referralsaasquatch.com>
2020-01-28 22:50:02 +0000

    Added article section to main for displaying posted tweets. Updated some class labels for more clarity and better global application

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   const maxChars = 140;
-  $(".counter").val(maxChars);
+  // $("#char-counter").val(maxChars);
 
   $("textarea").on("keyup keydown", function() {
-    const count = $(".counter");
+    const $count = $("#char-counter");
     const characters = $(this).val().length;
 
     if (characters > maxChars) {
-      count.addClass("over");
+      $count.addClass("over");
     } else {
-      count.removeClass("over");
+      $count.removeClass("over");
     }
-    count.text(maxChars - characters);
+    $count.text(maxChars - characters);
   });
 });
